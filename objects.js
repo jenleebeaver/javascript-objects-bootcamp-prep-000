079ae258908ac4a079fc718e10350b7663b8418a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-01-19 20:19:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ function updatePlaylist(playlist, name, song_title) {
   return playlist.assign ({}, {name: song_title})
 }
 
-function removeFromPlaylist(playlist, name)
-delete playlist.names;
-{return playlist}
+function removeFromPlaylist(playlist, name) {
+delete playlist.name;
+return playlist;
+}
